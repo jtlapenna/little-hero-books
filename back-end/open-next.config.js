11a155fec5ff125dc865eb34eb3143c534385762
Jeff <jeff@thepeakbeyond.com>
2025-10-30 07:58:17 -0700

    Fix OpenNext CJS export shape: module.exports = { default: {} }

--- back-end/open-next.config.js
+++ back-end/open-next.config.js
@@ -1,4 +1,4 @@
 // Minimal OpenNext config for Cloudflare Pages builds (CommonJS)
-// Using CJS avoids ESM import mismatches in some adapter versions
-module.exports = {};
+// Export a default property so the CLI sees config.default
+module.exports = { default: {} };
 
