5837a47325779cbb569ab054d2b71e868bf6099f
John Capogna <john@thepeakbeyond.com>
2025-10-29 17:47:44 -0700

    Add clarifying comments to build config

--- back-end/next.config.ts
+++ back-end/next.config.ts
@@ -4,10 +4,12 @@ const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
     // Disable ESLint during builds to unblock deployment - Cloudflare Pages
+    // This allows the site to deploy even with linting errors
     ignoreDuringBuilds: true,
   },
   typescript: {
     // Disable TypeScript checking during builds - Cloudflare Pages
+    // This allows the site to deploy even with type errors
     ignoreBuildErrors: true,
   },
 };
