c9ce7ce905ad778e738ca3ba952e5f85183490bf
Jeff <jeff@thepeakbeyond.com>
2025-11-03 11:46:52 -0800

    Add missing customization-utils module for webhook routes

---
+++ back-end/src/lib/customization-utils.ts
@@ -0,0 +1,10 @@
+/**
+ * Character specification normalization utilities
+ * TODO: Implement proper normalization logic based on requirements
+ */
+
+export function normalizeCharacterSpecs(specs: any): any {
+  // For now, return specs as-is
+  // TODO: Implement normalization logic (e.g., standardize keys, validate values)
+  return specs;
+}
