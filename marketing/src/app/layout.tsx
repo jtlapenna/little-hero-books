1840fdac272f457b5b0e15d9b2ccf330e66d3329
Jeff <jeff@thepeakbeyond.com>
2025-12-17 14:06:59 -0800

    Add Google Analytics 4 (GA4) to marketing site
    - Added GA4 tracking script with Measurement ID G-DEH39J706V
    - Script placed in head section of root layout as per Google's manual installation instructions
    - Will track page views and events on littleherolabs.com

--- marketing/src/app/layout.tsx
+++ marketing/src/app/layout.tsx
@@ -13,6 +13,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DEH39J706V"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DEH39J706V');
+            `,
+          }}
+        />
+      </head>
       <body className="antialiased">
         {children}
       </body>
